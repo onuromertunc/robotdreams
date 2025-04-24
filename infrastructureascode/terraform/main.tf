ab803eb625b8c5798bdb6f272bd4dd8228892cc1
Onur Omer TUNC <59953693+onuromertunc@users.noreply.github.com>
2025-04-24 18:47:21 +0300

    Update main.tf

--- infrastructureascode/terraform/main.tf
+++ infrastructureascode/terraform/main.tf
@@ -1,47 +1,47 @@
-# Azure Provider tanımlamasi
+
 provider "azurerm" {
   features {}
 
   # Tenant ID ve Subscription ID ayarları
-  tenant_id       = "4a70e5e7-3fa2-4e4f-bc0a-c3644fbd155c"
-  subscription_id = "d18ab524-8803-4dd7-b547-8f1a302f5b0a"
+  tenant_id       = "4a70e5e7-3fa2-4e4f-c3644fbd155c"
+  subscription_id = "d18ab524-8803--b547-8f1a302f5b0a"
 }
 
-# Resource Group Oluşturma
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "West US 2"  # Bölge güncellendi
+  location = "West US 2"  
 }
 
-# Sanal Ağ (VNet) Oluşturma
+
 resource "azurerm_virtual_network" "example_vnet" {
   name                = "example-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location  # Düzeltilmiş kaynak ismi
-  resource_group_name = azurerm_resource_group.example.name      # Düzeltilmiş kaynak ismi
+  location            = azurerm_resource_group.example.location  
+  resource_group_name = azurerm_resource_group.example.name      
 }
 
-# Alt Ağ (Subnet) Oluşturma
+
 resource "azurerm_subnet" "example_subnet" {
   name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name     # Düzeltilmiş kaynak ismi
+  resource_group_name  = azurerm_resource_group.example.name     
   virtual_network_name = azurerm_virtual_network.example_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Public IP Adresi Oluşturma
+
 resource "azurerm_public_ip" "example_public_ip" {
   name                = "example-public-ip"
-  location            = azurerm_resource_group.example.location  # Düzeltilmiş kaynak ismi
-  resource_group_name = azurerm_resource_group.example.name      # Düzeltilmiş kaynak ismi
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   allocation_method   = "Static"
 }
 
-# Ağ Arayüzü (Network Interface) Oluşturma
+
 resource "azurerm_network_interface" "example_nic" {
   name                = "example-nic"
-  location            = azurerm_resource_group.example.location  # Düzeltilmiş kaynak ismi
-  resource_group_name = azurerm_resource_group.example.name      # Düzeltilmiş kaynak ismi
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "internal"
@@ -51,11 +51,11 @@ resource "azurerm_network_interface" "example_nic" {
   }
 }
 
-# Ubuntu Sanal Makine Oluşturma
+
 resource "azurerm_linux_virtual_machine" "example_vm" {
   name                = "example-vm"
-  resource_group_name = azurerm_resource_group.example.name      # Düzeltilmiş kaynak ismi
-  location            = azurerm_resource_group.example.location  # Düzeltilmiş kaynak ismi
+  resource_group_name = azurerm_resource_group.example.name      
+  location            = azurerm_resource_group.example.location 
   size                = "Standard_DS1_v2"
   admin_username      = "azureuser"
 
@@ -84,7 +84,7 @@ resource "azurerm_linux_virtual_machine" "example_vm" {
   disable_password_authentication = true
 }
 
-# VM'nin Public IP'sini Çıktı Olarak Almak
+
 output "public_ip" {
   value = azurerm_public_ip.example_public_ip.ip_address
 }
